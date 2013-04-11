c6042a603181efe3450d92e619342838576c4338
augustoht <augustoh.teixeira@gmail.com>
2013-04-10 22:01:04 -0300

    Write some increase month scenario steps

--- features/step_definitions/calendar_steps.rb
+++ features/step_definitions/calendar_steps.rb
@@ -1,19 +1,19 @@
-When(/^I enter the page$/) do
+Given(/^I'm on the home page$/) do
   visit root_path
 end
 
-Then(/^I should see a calendar element$/) do
-  page.should have_selector 'div#calendar-id'
+Then(/^I should see a "(.*?)" element$/) do |arg1|
+  page.should have_selector arg1
 end
 
-Then(/^I should see the current month$/) do
+Then(/^I should see the present month$/) do
   current_month = I18n.l Date.today, format: '%b'
   within 'span#month-id' do
     page.should have_text current_month
   end
 end
 
-Then(/^I should see the current year$/) do
+Then(/^I should see the present year$/) do
   current_year = Date.today.strftime '%Y'
   within 'span#year-id' do
     page.should have_content current_year
@@ -23,10 +23,17 @@ end
 Then(/^I should see matching weekdays$/) do
   first_day = Date.today.at_beginning_of_month
   last_day = Date.today.at_end_of_month
-  rows = first_day.wday + last_day.day <= 35 ? 4 : 5
+  @rows = nil
+  if first_day.wday + last_day.day <= 28
+    @rows = 4
+  elsif first_day.wday + last_day.day <= 35
+    @rows = 5
+  else
+    @rows = 6
+  end
   current_day = first_day - first_day.wday
   present_month = Date.today.month
-  rows.times do |r|
+  @rows.times do |r|
     7.times do |c|
       element = page.find("##{r}-#{c}")
       element.should have_text current_day.day.to_s
@@ -43,6 +50,20 @@ Then(/^I should see matching weekdays$/) do
 end
 
 Then(/^I should see the correct rows amount$/) do
+  within '#calendar-id tbody' do
+    page.all('tr').count.should == @rows
+  end
+end
+
+Given(/^today is "(.*?)"$/) do |arg1|
+  page.execute_script "Holidays.__container__.lookup('controller:calendarIndex').set('currentDate', moment('#{arg1}'))"
+  page.find("#month-id").should have_text "Jan"
+end
+
+When(/^I click on the increase button$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
